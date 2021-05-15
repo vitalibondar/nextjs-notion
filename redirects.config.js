67aa8992254def6fe72bab8fe57fd735a8b00f37
Vitali Bondar <vitalibondar@users.noreply.github.com>
2021-05-15 20:56:10 +0300

    Update redirects.config.js

--- redirects.config.js
+++ redirects.config.js
@@ -1,7 +1,7 @@
 import { twitter, github, linkedin } from './site.config'
 
 const redirects = {
-  calendly: `https://calendly.com/wzulfikar`,
+//  calendly: `https://calendly.com/wzulfikar`,
   twitter: `https://twitter.com/${twitter}`,
   github: `https://github.com/${github}`,
   linkedin: `https://linkedin.com/in/${linkedin}`
